41c148434070a8dfa373a1374bb0dcec97ad440f
Jonathan Ramos <ramos.was.jonathan@gmail.com>
2019-12-23 13:05:45 -0300

    Criando os componentes iniciais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewUserComponent } from './components/new-user/new-user.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { NewCategoryComponent } from './components/new-category/new-category.component';
+import { ListCategoryComponent } from './components/list-category/list-category.component';
+import { NewProductComponent } from './components/new-product/new-product.component';
+import { ListProductComponent } from './components/list-product/list-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewUserComponent,
+    ListUserComponent,
+    NewCategoryComponent,
+    ListCategoryComponent,
+    NewProductComponent,
+    ListProductComponent
   ],
   imports: [
     BrowserModule,
